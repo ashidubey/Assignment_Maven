9ae582d2cca5e3fd31aaca02b8ccce4ce72d169c
ashidubey <ashi.dubey@knoldus.com>
2021-08-06 14:40:44 +0530

    Commit Maven Assignment

---
+++ cal-api/src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+    public static void Add(int num1 , int num2){
+        System.out.println("Addition is " + (num1 + num2));
+    }
+
+    public static void Subtract(int num1 , int num2){
+        System.out.println("Subtraction is " + (num1 - num2));
+    }
+
+    public static void Divide(int num1 , int num2){
+        System.out.println("Division is " + (num1 / num2));
+    }
+
+    public static void Product(int num1 , int num2){
+        System.out.println("Multiplication is " + (num1 * num2));
+    }
+}
---
+++ cal-impl/src/main/java/Application.java
@@ -0,0 +1,9 @@
+public class Application {
+    public static void main(String []args){
+        Calculator cal = new Calculator();
+        cal.Add(20, 30);
+        cal.Subtract(60, 20);
+        cal.Divide(620 , 20);
+        cal.Product(246, 80);
+    }
+}
